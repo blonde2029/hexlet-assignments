20330fdf429d3d7eba4e706f86c9831317f2c330
@hexlet/cli <support@hexlet.io>
2024-02-01 12:43:57 +0700

    submit java-spring-ru/http-crud

---
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -0,0 +1,56 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts")
+    public List<Post> index(@RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream().limit(limit).toList();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
+        return posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @PutMapping("/posts")
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var maybePost = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+        if (maybePost.isPresent()) {
+            var post = maybePost.get();
+            post.setBody(data.getBody());
+            post.setTitle(data.getTitle());
+            post.setId(data.getId());
+        }
+        return data;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+    // END
+}
